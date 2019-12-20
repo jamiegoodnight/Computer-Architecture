e451c7e84411e4bed309eefb86d1aea5e5d90528
Jamie Goodnight <goodnightlambda@gmail.com>
2019-12-20 10:59:48 -0500

    :construction: Initial commit and call.ls8 working from yesterday

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,12 +3,17 @@
 
 import sys
 
-HLT = 0b00000001
+# declare opcodes for clarity
+
 LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
-PUSH = 0b01000101
+HLT = 0b00000001
 POP = 0b01000110
+PUSH = 0b01000101
+CALL = 0b01010000
+RET = 0b00010001
+ADD = 0b10100000
 
 
 class CPU:
@@ -16,24 +21,23 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.pc = 0
         self.usage = 0
         self.sp = 7
 
-    def ram_read(self, pc):
-        print(self.ram[pc])
+    def ram_read(self, MAR):
+        return self.ram[MAR]
 
-    def ram_write(self, prog):
-        self.ram.append(prog)
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
+        return self.ram[MAR]
 
     def load(self, file):
         """Load a program into memory."""
         file = file
         address = 0
-
-        # For now, we've just hardcoded a program:
 
         with open(file) as f:
             for line in f:
@@ -45,6 +49,8 @@ class CPU:
                 self.ram[address] = val
                 address += 1
                 self.usage += 1
+
+        print(f"self.ram : {self.ram}")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -77,54 +83,67 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        self.reg[self.sp] = 244
-        cur_pos = self.ram[self.pc]
         running = True
-        # position_1 = self.ram[self.pc + 1]
-        # position_2 = self.ram[self.pc + 2]
         print(self.ram, "SELF.RAM")
         while running:
-            # print(self.usage, "USAGE", self.pc, "PC")
-            if self.ram[self.pc] == HLT:
-                if self.usage-1 == self.pc:
-                    print("*****HLT*****")
-                    running = False
+            ir = self.ram_read(self.pc)
+            position_1 = self.ram_read(self.pc + 1)
+            position_2 = self.ram_read(self.pc + 2)
 
-            elif self.ram[self.pc] == LDI:
-                position_1 = self.ram[self.pc + 1]
-                position_2 = self.ram[self.pc + 2]
+            if ir == HLT:
+                print("*****HLT*****")
+                running = False
+
+            if ir == LDI:
                 print("*****LDI*****")
                 self.reg[position_1] = position_2
                 self.pc += 3
 
-            elif self.ram[self.pc] == PRN:
+            elif ir == PRN:
                 print("*****PRN*****")
                 print(self.reg[position_1])
                 self.pc += 2
 
-            elif self.ram[self.pc] == MUL:
-                position_1 = self.ram[self.pc + 1]
-                position_2 = self.ram[self.pc + 2]
+            elif ir == MUL:
                 print("*****MUL*****")
                 print(self.reg[position_1]*self.reg[position_2])
                 self.pc += 3
 
-            elif self.ram[self.pc] == POP:
-                value = self.ram[self.reg[self.sp]]
-                self.reg[self.ram[self.pc + 1]] = value
-                self.reg[self.sp] += 1
-
-                self.pc += 2
-
-            elif self.ram[self.pc] == PUSH:
+            elif ir == PUSH:
+                print("*****PUSH*****")
                 self.reg[self.sp] -= 1
-                value = self.reg[self.ram[self.pc + 1]]
+                value = self.reg[position_1]
                 self.ram[self.reg[self.sp]] = value
-
                 self.pc += 2
+
+            elif ir == POP:
+                print("*****POP*****")
+                value = self.ram[self.reg[self.sp]]
+                self.reg[position_1] = value
+                self.reg[self.sp] += 1
+                self.pc += 2
+
+            elif ir == CALL:
+                print("*****CALL*****")
+                return_address = self.pc + 2
+                self.reg[self.sp] -= 1
+                self.ram[self.reg[self.sp]] = return_address
+
+                reg_num = self.ram[self.pc + 1]
+                sub_address = self.reg[reg_num]
+                self.pc = sub_address
+
+            elif ir == RET:
+                print("*****RET*****")
+                return_address = self.ram[self.reg[self.sp]]
+                self.reg[self.sp] += 1
+                self.pc = return_address
+
+            elif ir == ADD:
+                print("*****ADD*****")
+                value = self.reg[position_1] + self.reg[position_2]
+                self.reg[position_1] = value
+                self.pc += 3
 
             else:
                 print(f"Unknown instruction in RAM at: {self.pc}")
-
-            # self.pc += 1
-        pass
